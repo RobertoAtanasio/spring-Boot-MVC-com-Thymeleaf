59d6c38a07a7bdd942bdad56401d3204dc973812
Roberto Atanásio <roberto.atanasio.pl@gmail.com>
2019-06-29 16:39:08 -0300

    Mapeando as entidades'

---
+++ src/main/java/com/rapl/spring/boot/domain/Departamento.java
@@ -0,0 +1,38 @@
+package com.rapl.spring.boot.domain;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "departamentos")
+public class Departamento extends AbstractEntity<Long> {
+
+	@Column(name = "nome", nullable = false, unique = true, length = 60)
+	private String nome;
+	
+	// aqui fica o atributo "departamento" que faz referência ao departmento na classe cargo
+	@OneToMany(mappedBy = "departamento")
+	private List<Cargo> cargos;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Cargo> getCargos() {
+		return cargos;
+	}
+
+	public void setCargos(List<Cargo> cargos) {
+		this.cargos = cargos;
+	}
+	
+}
